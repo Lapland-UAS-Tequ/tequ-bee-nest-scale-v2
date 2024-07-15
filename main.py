55829803beb8bb63bf77bf0d3536923a3c5bb295
juhaautioniemi <juha.autioniemi@lapinamk.fi>
2024-07-15 10:45:46 +0300

    minor modifications

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from umqtt.simple import MQTTClient
 from machine import UART
 from machine import Pin
+import machine
 import xbee
 import time
 import network
 import ujson
 from functions import createPayload, readData, log
 
-sleepTime_ms = 21600000
+log("Main: Tequ - Bee Scale v2 - [2024-07-15]")
+watchdog = machine.WDT(timeout=180000, response=machine.CLEAN_SHUTDOWN)
 
+sleepTime_ms = 3600000
 cellular = network.Cellular()
 cellular.active(True)
-while not cellular.isconnected():
-    log("Attempting connection...")
-    time.sleep_ms(1000)
+# while not cellular.isconnected():
+#    log("Attempting connection...")
+#    time.sleep_ms(1000)
 
 # Sleep button currently doesn't work, since internal pull-up resistor doesn't appear to work
 #wakeButton = Pin('D8', mode=Pin.IN, pull=Pin.)
@@ -40,7 +43,7 @@ topic = ""
 client = MQTTClient(client_id=client_id, server=server_url, port=port, user=username, password=password, keepalive=60)
 
 while True:
-    cellular.active(True)
+    #cellular.active(True)
     log("Getting sensor data...")
     boot_count += 1
     payload = createPayload(x, readData(uart), boot_count)
@@ -57,6 +60,7 @@ while True:
     log("Data sent")
     log("Sent payload: "+str(payload))
     log("Going to sleep...\n")
+    watchdog.feed()
     cellular.active(False)
     sleep_ms = x.sleep_now(sleepTime_ms, False)
     log("Slept for: "+str(sleep_ms))
