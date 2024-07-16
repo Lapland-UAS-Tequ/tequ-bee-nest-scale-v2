1fade818e7210a964a6e2f1af395407979f46a85
juhaautioniemi <juha.autioniemi@lapinamk.fi>
2024-07-16 13:31:51 +0300

    updated software to support watchdog and did some power saving tweaks

--- lib/functions.py
+++ lib/functions.py
@@ -1,4 +1,6 @@
 import time
+from sys import print_exception
+import network
 
 def createPayload(xbee, sensor_values, boot_count):
     # Get current time
@@ -30,7 +32,7 @@ def createPayload(xbee, sensor_values, boot_count):
                     "v": internalTempXBee
                 },
                 "7": {
-                    "sd": xbee.atcmd('%V')
+                    "v": xbee.atcmd('%V')
                 }
             }
         }
@@ -44,24 +46,29 @@ def readData(uartObject):
     count = 0
 
     # Cycle a few readings to get more accurate measurements
-    log("Reading data...")
-    while count < 5:
+    log("Reading sensor data...")
+    while count < 30:
         count += 1
         data = uartObject.readline()
+
         # Check if data None
-        if data == None:
+        if count > 30:
+            log("Count over 30 return none...")
+            data = None
+        elif data is None:
             log("Data none, retrying...")
-            count -= 1
-            continue
-        data = data.decode().split(',')
-        # Check if data good, retry if not.
-        # Data is good if the split data packet has 7 elements
-        # This value should be changed if the Sparkfun Openscale board's configuration is changed.
-        if len(data) != 7:
-            log("Text or corrupted data, retrying...")
-            print(data)
-            count -= 1
-    log("Good data found")
+        else:
+            data = data.decode().split(',')
+            # Check if data good, retry if not.
+            # Data is good if the split data packet has 7 elements
+            # This value should be changed if the Sparkfun Openscale board's configuration is changed.
+            if len(data) != 7:
+                log("Text or corrupted data, retrying...")
+                print(data)
+            else:
+                log("Good data found")
+                print(data)
+                break
 
     return data
 
@@ -72,3 +79,41 @@ def log(string):
         logTime[0], logTime[1], logTime[2], logTime[3], logTime[4], logTime[5]
     )
     print("%s : %s" % (logTime, string))
+
+
+def configure_xbee(xbeeObj):
+    try:
+        xbeeObj.atcmd('BD', 7)
+        xbeeObj.atcmd('NB', 0)
+        xbeeObj.atcmd('SB', 0)
+        xbeeObj.atcmd('SM', 0)
+        xbeeObj.atcmd('AM', 0)
+        xbeeObj.atcmd('AN', "internet")
+        xbeeObj.atcmd('N#', 0)
+        xbeeObj.atcmd('CP', 0)  # 0 = autodetect 1 = no profile
+        xbeeObj.atcmd("BM", b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+        xbeeObj.atcmd('DO', 17)
+        xbeeObj.atcmd('MO', 7)
+        xbeeObj.atcmd('D0', 0)
+        xbeeObj.atcmd('D1', 0)
+        xbeeObj.atcmd('D2', 0)
+        xbeeObj.atcmd('D3', 2)
+        xbeeObj.atcmd('D4', 7)  # secondary UART TX
+        xbeeObj.atcmd('D5', 1)
+        xbeeObj.atcmd('D6', 0)
+        xbeeObj.atcmd('D7', 4)
+        xbeeObj.atcmd('D8', 0)  # SLEEP_REQUEST (IN)
+        xbeeObj.atcmd('D9', 1)
+        xbeeObj.atcmd('P0', 1)
+        xbeeObj.atcmd('P1', 0)
+        xbeeObj.atcmd('P2', 7)  # secondary UART RX
+        xbeeObj.atcmd('P3', 1)
+        xbeeObj.atcmd('P4', 1)
+        xbeeObj.atcmd('%L', 3150)
+        xbeeObj.atcmd('%M', 100)
+        xbeeObj.atcmd('PS', 1)
+        xbeeObj.atcmd('AP', 4)
+        log("Configuring XBee... OK")
+    except Exception as e:
+        log("Configuring XBee... FAILED")
+        print_exception(e)
--- main.py
+++ main.py
@@ -1,72 +1,97 @@
-from umqtt.simple import MQTTClient
-from machine import UART
-from machine import Pin
+# import libraries
 import machine
 import xbee
 import time
 import network
 import ujson
-from functions import createPayload, readData, log
-
-log("Main: Tequ - Bee Scale v2 - [2024-07-15]")
-watchdog = machine.WDT(timeout=180000, response=machine.CLEAN_SHUTDOWN)
-
-sleepTime_ms = 3600000
-cellular = network.Cellular()
-cellular.active(True)
-# while not cellular.isconnected():
-#    log("Attempting connection...")
-#    time.sleep_ms(1000)
-
-# Sleep button currently doesn't work, since internal pull-up resistor doesn't appear to work
-#wakeButton = Pin('D8', mode=Pin.IN, pull=Pin.)
-
-# Start serial connection to Sparkfun Openscale
-uart = UART(1, 9600)
-uart.init(9600, bits=8, parity=None, stop=1, timeout=1000)
+from umqtt.simple import MQTTClient
+from functions import createPayload, readData, log, configure_xbee
+from sys import print_exception
 
 boot_count = 0
+log("Main: Tequ - Bee Scale - [2024-07-16]")
+log("Configuring watchdog...")
+watchdog = machine.WDT(timeout=120000, response=machine.CLEAN_SHUTDOWN)
+
+# Initialize cellular connection
+cellular = network.Cellular()
+cellular.active(True)
+
+# Initialize XBee
 x = xbee.XBee()
+configure_xbee(x)
 
-file = open('config.json')
-settings = {}
-settings = ujson.loads(file.read())
+# Start serial connection to Sparkfun Openscale
+log("Opening serial connection to SparkFun OpenScale...")
+uart = machine.UART(1, 9600)
 
+# Read configuration file
+try:
+    file = open('config.json')
+    settings = {}
+    settings = ujson.loads(file.read())
+    log("Opening configuration file... OK")
+except Exception as e:
+    log("Opening configuration file... FAILED")
+    print_exception(e)
+
+# Set variables
+sleep_time_ms = int(settings["sleep_interval_ms"])
 imei = str(x.atcmd('IM'))
 client_id = imei
-server_url = settings["url"]
-port = int(settings["port"])
-password = settings["password"]
+server_url = settings["mqtt_url"]
+port = int(settings["mqtt_port"])
+password = settings["mqtt_password"]
 username = imei
-topic = ""
+topic = "tequ/type/bee-scale/id/"+imei+"/event/data"
 
-client = MQTTClient(client_id=client_id, server=server_url, port=port, user=username, password=password, keepalive=60)
+# Initialize MQTT client
+client = MQTTClient(client_id=client_id, server=server_url, port=port, user=username, password=password, keepalive=15)
 
 while True:
-    #cellular.active(True)
-    log("Getting sensor data...")
-    boot_count += 1
-    payload = createPayload(x, readData(uart), boot_count)
-    log("Sensor data got")
-    payload = ujson.dumps(payload)
-    log("Establishing cellular connection...")
-    while not cellular.isconnected():
-        log("Attempting connection...")
-        time.sleep_ms(1000)
-    log("Connected")
-    log("Sending data...")
-    client.connect()
-    client.publish(topic, payload)
-    log("Data sent")
-    log("Sent payload: "+str(payload))
-    log("Going to sleep...\n")
-    watchdog.feed()
-    cellular.active(False)
-    sleep_ms = x.sleep_now(sleepTime_ms, False)
-    log("Slept for: "+str(sleep_ms))
-    if x.wake_reason() is xbee.PIN_WAKE:
-        log("Woke up early on pin toggle")
-    elif x.wake_reason() is xbee.RTC_WAKE:
-        log("Woke up on time")
-    else:
-        log("Woke up somehow else")
+    try:
+        boot_count += 1
+        if not cellular.active():
+            log("Enable cellular connection")
+            cellular.active(True)
+
+        # Enable second UART connection
+        x.atcmd('P2', 7)
+        x.atcmd('D4', 7)
+        uart.init(9600, bits=8, parity=None, stop=1, timeout=1000)
+        data = readData(uart)
+
+        while 1:
+            if cellular.isconnected():
+                log("Cellular connected")
+
+                if data is None:
+                    log("Sparkfun OpenScale sensor not working or not connected")
+                else:
+                    try:
+                        payload = ujson.dumps(createPayload(x, data, boot_count))
+                        log("Data packet created: %s " % str(payload))
+                        log("Connecting to MQTT server...")
+                        client.connect()
+                        client.publish(topic, payload)
+                        log("Sending data to MQTT server...OK")
+                        log("Sent payload: %s" % payload)
+                    except Exception as e:
+                        log("Processing sensor data... FAILED")
+                        print_exception(e)
+                    finally:
+                        break
+            else:
+                log("Waiting for cellular connection...")
+
+        watchdog.feed()
+
+    finally:
+        log("Going to sleep for %d ms..." % sleep_time_ms)
+        # Disable second UART connection
+        uart.deinit()
+        x.atcmd('P2', 0)
+        x.atcmd('D4', 0)
+        cellular.active(False)
+        sleep_ms = x.sleep_now(sleep_time_ms, False)
+        log("Slept %d ms, wake reason: %s" % (sleep_ms, x.wake_reason()))
